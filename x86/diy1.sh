98177d1794ffd3c8f0d1a828a3b339e0a4c1e4db
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-07-13 07:31:10 +0800

    Update diy1.sh

--- x86/diy1.sh
+++ x86/diy1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-
-echo "开始 DIY1 配置……"
-
+echo "========================="
+echo "……开始 DIY1 配置……"
+echo "========================="
 
 
 
@@ -17,44 +17,5 @@ echo "开始 DIY1 配置……"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 
-#svn co 复制 仓库下的文件夹 git clone 复制整个仓库
-# vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-
-# serverchan通知
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-
-# 关机
-git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-
-# unblockneteasemusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-
-#adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-
-# filebrowser
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
-
-#passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-
-# passwall2
-# svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-
-# smartdns
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/luci-app-smartdns
-
-# SSRP
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-
-# openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-
-# Themes
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
-echo "DIY1 配置完成……"
+echo "========================="
+echo "……DIY1 配置完成……"
