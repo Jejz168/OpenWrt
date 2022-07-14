f7e142915f598b0d5c15351e0bb8182324961e88
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-07-14 12:32:30 +0800

    Update diy-part2.sh

--- x86/diy-part2.sh
+++ x86/diy-part2.sh
@@ -59,10 +59,13 @@ pushd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
 popd
 
-# argon
+# argon 主题
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# infinityfreedom 主题
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
 
 #主机名右上角符号❤
 #sed -i 's/❤/❤/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
