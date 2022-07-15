e8e81fc4b8f709d0887b4fcd384e6c79fdd2d8fd
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-07-15 21:16:49 +0800

    Update diy-part2.sh

--- armv8/diy-part2.sh
+++ armv8/diy-part2.sh
@@ -7,6 +7,9 @@
 # Blog: https://p3terx.com
 #===============================================
 
+#允许ROOT编译
+export FORCE_UNSAFE_CONFIGURE=1
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.8.8/g' package/base-files/files/bin/config_generate
 
