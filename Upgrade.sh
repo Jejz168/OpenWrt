cb967e45ca39815bfa13b5282e51a89d1fb0f566
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-07-20 10:15:33 +0800

    Create Upgrade.sh

---
+++ Upgrade.sh
@@ -0,0 +1,10 @@
+#tmp扩容到1g，升级重启后恢复
+mount -t tmpfs -o remount,size=1G tmpfs /tmp
+
+##下载解压的文件放/tmp/目录
+##再运行下面二选一
+#保留配置升级
+sysupgrade /tmp/openwrt_x86-64_5.15.55_uefi.img
+
+#不保留配置升级
+sysupgrade -n /tmp/openwrt_x86-64_5.15.55_uefi.img
