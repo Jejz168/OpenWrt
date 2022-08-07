b2ed0cf841ffc3891286aeeb37b28fd2cd025aaa
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-08-07 09:20:22 +0800

    Update diy-part2.sh

--- armv8/diy-part2.sh
+++ armv8/diy-part2.sh
@@ -65,7 +65,11 @@ svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-
 # filebrowser 文件浏览器
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
 
+# ssr
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
 # passwall
+git clone --depth=1 -b packages https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
 
 # passwall2
