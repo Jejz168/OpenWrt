9a9fd9294151ba24fc7de794b9aaa7e2bbac012b
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-08-10 17:17:11 +0800

    Update diy-part2.sh

--- armv8/diy-part2.sh
+++ armv8/diy-part2.sh
@@ -134,7 +134,7 @@ sed -i 's/<a href=\"https:\/\/github.com\/coolsnowwolf\/luci\">/<a>/g' feeds/luc
 # 晶晨宝盒
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 sed -i "s|https.*/OpenWrt|https://github.com/Jejz168/OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|opt/kernel|https://github.com/Jejz168/OpenWrt/tree/main/backup/kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|opt/kernel|backup/kernel|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # 修改makefile
