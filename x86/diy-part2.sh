21ccbbd82d9786ef653f2380abf54f4f3fac132b
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-08-13 15:59:09 +0800

    Update diy-part2.sh

--- x86/diy-part2.sh
+++ x86/diy-part2.sh
@@ -84,8 +84,9 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser pack
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-smartdns package/luci-app-smartdns
 
 # turboacc 去dns
-rm -rf feeds/luci/applications/luci-app-turboacc
-svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/patch/luci-app-turboacc package/luci-app-turboacc
+sed -i '60,70d' feeds/luci/applications/luci-app-turboacc/Makefile
+sed -i '54,78d' feeds/luci/applications/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+sed -i '7d;15d;21d' feeds/luci/applications/luci-app-turboacc/luasrc/view/turboacc/turboacc_status.htm
 
 # passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
