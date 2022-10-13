ab15212f64926f649f223d60049cd4a787de7648
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2022-10-13 18:03:45 +0800

    Update diy-part2.sh

--- armv8/diy-part2.sh
+++ armv8/diy-part2.sh
@@ -93,6 +93,9 @@ svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/patch/luci-ap
 rm -rf feeds/luci/applications/luci-app-netdata
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
 
+# ddns-go 动态域名
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
 # eqos 限速
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-eqos package/luci-app-eqos
 
