fe83e97769e2d2676219b7a05876df41138768a9
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-03-26 12:22:24 +0800

    Update diy-part2.sh

--- x86/diy-part2.sh
+++ x86/diy-part2.sh
@@ -61,6 +61,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-n
 # sed -i 's/PKG_HASH:=.*/PKG_HASH:=e35824e19e8acc06296ce6bfa78a14a6f3ee8f42a965f7762b7056b506457a29/g' feeds/Jejz/xray-core/Makefile
 # cp -f $GITHUB_WORKSPACE/personal/hysteria/* feeds/Jejz/hysteria
 rm -rf feeds/packages/utils/v2dat
+cp -f $GITHUB_WORKSPACE/personal/chinadns-ng/* feeds/Jejz/chinadns-ng
 
 # svn co 复制 仓库下的文件夹 git clone 复制整个仓库
 # ikoolproxy
