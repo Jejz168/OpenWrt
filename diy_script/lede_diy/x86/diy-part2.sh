09382c1f1988706de37e051232190217ff30bde0
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-08-18 22:45:21 +0800

    Update diy-part2.sh

--- diy_script/lede_diy/x86/diy-part2.sh
+++ diy_script/lede_diy/x86/diy-part2.sh
@@ -47,11 +47,8 @@ sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.te
 
 # 修改 argon 为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/luci/Makefile
-sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/*/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/*/Makefile
 
 ##切换为samba4
 # sed -i 's/luci-app-samba/luci-app-samba4/g' package/lean/autosamba/Makefile
