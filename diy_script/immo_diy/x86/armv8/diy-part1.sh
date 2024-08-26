d5f3847b1ecea7262cf63c3b1f28575e0066b967
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-08-26 12:44:44 +0800

    Create diy-part1.sh

---
+++ diy_script/immo_diy/x86/armv8/diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script part 1
+# File name: diy-part1.sh
+# Lisence: MIT
+# By: Jejz
+#===============================================
+
+
+# Add a feed source
+# sed -i "/helloworld/d" feeds.conf.default
+# sed -i '$a src-git Jejz https://github.com/Jejz168/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
