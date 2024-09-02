398fc6837b21913247d57c4cbda05aacd36fce32
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-09-02 22:05:38 +0800

    Update diy-part1.sh

--- diy_script/immo_diy/x86/diy-part1.sh
+++ diy_script/immo_diy/x86/diy-part1.sh
@@ -7,10 +7,10 @@
 #===============================================
 
 # Add a feed source
-#sed -i "/helloworld/d" feeds.conf.default
-#sed -i '$a src-git Jejz https://github.com/Jejz168/openwrt-packages' feeds.conf.default
+sed -i "/helloworld/d" feeds.conf.default
+# sed -i '$a src-git Jejz https://github.com/Jejz168/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-# sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
+sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 
 
 
