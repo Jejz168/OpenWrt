5df1cc2f91af1ef7808312ef21416d7a2acda33a
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-09-25 07:58:15 +0800

    更新 diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -120,7 +120,7 @@ merge_package main https://github.com/xiaorouji/openwrt-passwall package/custom 
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
 
 # nekoclash
-git clone --depth=1 -b neko https://github.com/Thaolga/luci-app-nekoclash package/nekoclash
+# git clone --depth=1 -b neko https://github.com/Thaolga/luci-app-nekoclash package/nekoclash
 
 # openclash
 rm -rf feeds/luci/applications/luci-app-openclash
