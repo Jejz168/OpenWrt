47512f67d7cf777b04e873d4eb457c9585060947
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-11-23 12:54:57 +0800

    Update diy23-part2.sh

--- diy_script/lede_diy/x86/diy23-part2.sh
+++ diy_script/lede_diy/x86/diy23-part2.sh
@@ -77,7 +77,7 @@ merge_package master https://github.com/xiangfeidexiaohuo/extra-ipk package/cust
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
 # chatgpt
-git clone --depth=1 https://github.com/sirpdboy/luci-app-chatgpt-web package/luci-app-chatgpt
+# git clone --depth=1 https://github.com/sirpdboy/luci-app-chatgpt-web package/luci-app-chatgpt
 
 # lucky 大吉
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
