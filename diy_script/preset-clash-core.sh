b0355175deaffdae35fde5ff24ec97876f4e9d6b
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-12-13 21:21:46 +0800

    更新 preset-clash-core.sh

--- diy_script/preset-clash-core.sh
+++ diy_script/preset-clash-core.sh
@@ -5,9 +5,11 @@ mkdir -p files/etc/openclash/core
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+GEO_MMDB_URL="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
 
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+wget -qO- $GEO_MMDB_URL > files/etc/openclash/Country.mmdb
 
 chmod +x files/etc/openclash/core/clash*
