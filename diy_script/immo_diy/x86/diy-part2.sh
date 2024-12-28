b32fd810a4059c5628ec6b87cbce5e1d1f184a2d
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-12-29 07:55:04 +0800

    更新 diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -54,7 +54,7 @@ sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
 # filebrowser luci-app-pushbot
 rm -rf feeds/packages/utils/filebrowser
 rm -rf feeds/luci/applications/luci-app-filebrowser
-merge_package main https://github.com/xiangfeidexiaohuo/2305-ipk package/custom luci-app-adguardhome luci-app-pushbot op-fileBrowser
+merge_package main https://github.com/xiangfeidexiaohuo/2305-ipk package/custom luci-app-adguardhome luci-app-pushbot op-fileBrowser luci-app-poweroff
 
 # ddns-go 动态域名
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
