3f599eaabff175e47427a5e91e6645e69277cd38
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-12-28 21:09:37 +0800

    Update diy-part2.sh

--- diy_script/lede_diy/armv8/diy-part2.sh
+++ diy_script/lede_diy/armv8/diy-part2.sh
@@ -53,9 +53,9 @@ rm -rf feeds/packages/utils/v2dat
 
 # merge_package 复制 仓库下的文件夹 git clone 复制整个仓库
 # vssr adguardhome turboacc去dns
-rm -rf feeds/luci/applications/luci-app-turboacc
+# rm -rf feeds/luci/applications/luci-app-turboacc
 rm -rf package/feeds/packages/adguardhome
-merge_package master https://github.com/xiangfeidexiaohuo/extra-ipk package/custom luci-app-adguardhome patch/luci-app-turboacc patch/wall-luci/lua-maxminddb patch/wall-luci/luci-app-vssr
+merge_package master https://github.com/xiangfeidexiaohuo/extra-ipk package/custom luci-app-adguardhome patch/wall-luci/lua-maxminddb patch/wall-luci/luci-app-vssr
 
 # ddns-go 动态域名
 # git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
