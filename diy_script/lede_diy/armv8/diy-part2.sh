620e4051f9e238d73a766f5f459df37352c89373
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-12-29 15:06:03 +0800

    Update diy-part2.sh

--- diy_script/lede_diy/armv8/diy-part2.sh
+++ diy_script/lede_diy/armv8/diy-part2.sh
@@ -131,7 +131,7 @@ git_clone https://github.com/sirpdboy/luci-theme-opentopd
 git_clone https://github.com/thinktip/luci-theme-neobird
 
 # 更改argon主题背景
-cp -f $GITHUB_WORKSPACE/personal/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/personal/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 主机名右上角符号❤
 # sed -i 's/❤/❤/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
