06e0dd9eca43d3c37c74636806238b6902a38e85
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-12-30 18:10:09 +0800

    Update function.sh

--- diy_script/function.sh
+++ diy_script/function.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
 # 拉取仓库文件夹
-merge_package() {
-	# 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
-	# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-	# 示例:
-	# merge_package master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
-	# merge_package master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
-	if [[ $# -lt 3 ]]; then
-		echo "Syntax error: [$#] [$*]" >&2
-		return 1
-	fi
-	trap 'rm -rf "$tmpdir"' EXIT
-	branch="$1" curl="$2" target_dir="$3" && shift 3
-	rootdir="$PWD"
-	localdir="$target_dir"
-	[ -d "$localdir" ] || mkdir -p "$localdir"
-	tmpdir="$(mktemp -d)" || exit 1
-        echo "开始下载：$(echo $curl | awk -F '/' '{print $(NF)}')"
-	git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-	cd "$tmpdir"
-	git sparse-checkout init --cone
-	git sparse-checkout set "$@"
-	# 使用循环逐个移动文件夹
-	for folder in "$@"; do
-		mv -f "$folder" "$rootdir/$localdir"
-	done
-	cd "$rootdir"
-}
-
 drop_package(){
 	find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
 }
@@ -38,7 +10,6 @@ merge_feed(){
 	./scripts/feeds install -a -p $1
 }
 
-# 拉取仓库文件夹
 color() {
     case $1 in
         cr) echo -e "\e[1;31m$2\e[0m" ;;
