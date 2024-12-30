4eb614d3959e3f39151f43c8093e277b0379ccb2
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2024-12-30 18:25:46 +0800

    Create diy-part2.sh

---
+++ diy_script/openwrt_diy/diy-part2.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script part 2
+# File name: diy-part2.sh
+# Lisence: MIT
+# By: Jejz
+#===============================================
+
+echo "开始 DIY2 配置……"
+echo "========================="
+
+# Git稀疏克隆，只克隆指定目录到本地
+chmod +x $GITHUB_WORKSPACE/diy_script/function.sh
+source $GITHUB_WORKSPACE/diy_script/function.sh
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.8.3/g' package/base-files/files/bin/config_generate
+
+# 设置ttyd免帐号登录
+sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 默认 shell 为 bash
+sed -i 's/\/bin\/ash/\/bin\/bash/g' package/base-files/files/etc/passwd
+
+# 精简 UPnP 菜单名称
+sed -i 's#\"title\": \"UPnP IGD \& PCP/NAT-PMP\"#\"title\": \"UPnP\"#g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
+
+# 优化socat中英翻译
+sed -i 's/仅IPv6/仅 IPv6/g' package/feeds/luci/luci-app-socat/po/zh_Hans/socat.po
+
+# samba解除root限制
+sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.template
+
+# 取消bootstrap为默认主题
+sed -i '/set_opt main.mediaurlbase \/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/*/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/*/Makefile
+
+# 修复上移下移按钮翻译
+sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
+sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
+
+# 修复procps-ng-top导致首页cpu使用率无法获取
+sed -i 's#top -n1#\/bin\/busybox top -n1#g' feeds/luci/modules/luci-base/root/usr/share/rpcd/ucode/luci
+
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+# 报错修复
+# rm -rf feeds/packages/utils/v2dat
+
+# 添加整个源仓库(git_clone)/添加源仓库内的指定目录(clone_dir)/添加源仓库内的所有目录(clone_all)
+# filebrowser luci-app-pushbot
+rm -rf feeds/packages/net/adguardhome
+clone_dir main https://github.com/xiangfeidexiaohuo/2305-ipk luci-app-adguardhome luci-app-pushbot luci-app-poweroff
+
+# 替换immortalwrt插件
+clone_dir master https://github.com/immortalwrt/luci luci-app-socat luci-app-syncdial luci-app-eqos luci-app-nps luci-app-frpc luci-app-frps luci-app-hd-idle luci-app-socat luci-app-n2n luci-app-softethervpn
+
+# ddns-go 动态域名
+# clone_all https://github.com/sirpdboy/luci-app-ddns-go
+
+# chatgpt
+# git_clone https://github.com/sirpdboy/luci-app-chatgpt-web luci-app-chatgpt
+
+# lucky 大吉
+clone_all https://github.com/gdy666/luci-app-lucky
+
+# ddnsto
+clone_dir main https://github.com/linkease/nas-packages-luci luci-app-ddnsto
+clone_dir master https://github.com/linkease/nas-packages ddnsto
+
+# OpenAppFilter 应用过滤
+clone_all https://github.com/sbwml/OpenAppFilter
+
+# autotimeset 定时
+# git_clone https://github.com/sirpdboy/luci-app-autotimeset
+
+# dockerman
+# clone_dir https://github.com/lisaac/luci-app-dockerman luci-app-dockerman
+
+# unblockneteasemusic
+# git_clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
+
+# smartdns
+git_clone https://github.com/pymumu/luci-app-smartdns luci-app-smartdns
+git_clone https://github.com/pymumu/openwrt-smartdns smartdns
+
+# mosdns
+clone_all v5 https://github.com/sbwml/luci-app-mosdns
+
+# alist
+git_clone https://github.com/sbwml/packages_lang_golang golang
+clone_all https://github.com/sbwml/luci-app-alist
+
+# ssr-plus
+clone_all https://github.com/fw876/helloworld
+
+# passwall
+clone_all https://github.com/xiaorouji/openwrt-passwall-packages
+clone_all https://github.com/xiaorouji/openwrt-passwall
+
+# passwall2
+# clone_all https://github.com/xiaorouji/openwrt-passwall2
+
+# mihomo
+clone_all https://github.com/morytyann/OpenWrt-mihomo
+
+# homeproxy
+git_clone https://github.com/immortalwrt/homeproxy luci-app-homeproxy
+
+# openclash
+clone_dir master https://github.com/vernesong/OpenClash luci-app-openclash
+# clone_dir dev https://github.com/vernesong/OpenClash luci-app-openclash
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd $destination_dir/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
+# argon 主题
+# git_clone https://github.com/kiddin9/luci-theme-edge
+git_clone https://github.com/jerrykuku/luci-theme-argon
+git_clone https://github.com/jerrykuku/luci-app-argon-config
+# clone_all https://github.com/sbwml/luci-theme-argon
+
+# 更改argon主题背景
+cp -f $GITHUB_WORKSPACE/personal/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+# 修改主题多余版本信息
+sed -i 's|<a class="luci-link" href="https://github.com/openwrt/luci"|<a|g' $destination_dir/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+sed -i 's|<a class="luci-link" href="https://github.com/openwrt/luci"|<a|g' $destination_dir/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">|<a>|g' $destination_dir/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">|<a>|g' $destination_dir/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+
+# 显示增加编译时间
+# sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt R$(TZ=UTC-8 date +'%-m.%-d') (By @Jejz build $(TZ=UTC-8 date "+%Y-%m-%d %H:%M"))'/g" package/base-files/files/etc/openwrt_release
+
+# 修改欢迎banner
+cp -f $GITHUB_WORKSPACE/personal/banner package/base-files/files/etc/banner
+# wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/Jejz168/OpenWrt/main/personal/banner
+
+# 修改makefile
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/lang\/golang\/golang\-package\.mk/include \$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang\-package\.mk/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
+find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
+
+# 设置 nlbwmon 独立菜单
+sed -i 's/services\/nlbw/nlbw/g; /path/s/admin\///g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+sed -i 's/services\///g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
+
+# 调整位置
+sed -i 's/services/nas/g' feeds/luci/applications/luci-app-p910nd/root/usr/share/luci/menu.d/luci-app-p910nd.json
+sed -i 's/services/nas/g' feeds/luci/applications/luci-app-aria2/root/usr/share/luci/menu.d/luci-app-aria2.json
+sed -i 's/services/nas/g' feeds/luci/applications/luci-app-filebrowser/root/usr/share/luci/menu.d/luci-app-filebrowser.json
+sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+
+# 更改 ttyd 顺序和名称
+sed -i '3a \		"order": 10,' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+sed -i 's,终端,TTYD 终端,g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+
+# 重命名
+sed -i 's,frp 服务器,Frp 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
+sed -i 's,frp 客户端,Frp 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
+
+# 修改插件名字
+# sed -i 's/"挂载 SMB 网络共享"/"挂载共享"/g' `grep "挂载 SMB 网络共享" -rl ./`
+# sed -i 's/"Argon 主题设置"/"Argon 设置"/g' `grep "Argon 主题设置" -rl ./`
+# sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' `grep "阿里云盘 WebDAV" -rl ./`
+# sed -i 's/"USB 打印服务器"/"USB 打印"/g' `grep "USB 打印服务器" -rl ./`
+# sed -i 's/"网络存储"/"存储"/g' `grep "网络存储" -rl ./`
+# sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `grep "Turbo ACC 网络加速" -rl ./`
+# sed -i 's/"实时流量监测"/"流量"/g' `grep "实时流量监测" -rl ./`
+# sed -i 's/"KMS 服务器"/"KMS激活"/g' `grep "KMS 服务器" -rl ./`
+# sed -i 's/"TTYD 终端"/"命令窗"/g' `grep "TTYD 终端" -rl ./`
+# sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -rl ./`
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+echo "========================="
+echo " DIY2 配置完成……"
