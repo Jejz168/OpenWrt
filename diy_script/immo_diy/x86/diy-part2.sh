b3e1ff6a54af22907448988e33a05f4fae28874b
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-01-01 16:54:20 +0800

    Update diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -126,7 +126,8 @@ sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank
 sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">|<a>|g' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
 
 # 显示增加编译时间
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt R$(TZ=UTC-8 date +'%-m.%-d') (By @Jejz build $(TZ=UTC-8 date "+%Y-%m-%d %H:%M"))'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='ImmortalWrt By @Jejz'/g"  package/base-files/files/etc/openwrt_release
+sed -i "s/OPENWRT_RELEASE=.*/OPENWRT_RELEASE='ImmortalWrt R$(TZ=UTC-8 date +'%-m.%-d') (By @Jejz build $(TZ=UTC-8 date "+%Y-%m-%d %H:%M"))'/g"  package/base-files/files/usr/lib/os-release
 
 # 修改欢迎banner
 cp -f $GITHUB_WORKSPACE/diy_script/immo_diy/x86/99-default-settings package/emortal/default-settings/files/99-default-settings
