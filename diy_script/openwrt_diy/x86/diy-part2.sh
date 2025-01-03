b9d1dabc4df438ed65fd237fd3b99f8c38ee52e4
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-01-03 16:58:12 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -52,10 +52,10 @@ rm -rf feeds/packages/net/adguardhome
 clone_dir main https://github.com/xiangfeidexiaohuo/2305-ipk luci-app-adguardhome luci-app-pushbot luci-app-poweroff
 
 # 替换immortalwrt插件
-clone_dir master https://github.com/immortalwrt/luci luci-app-zerotier luci-app-openvpn-server luci-app-ipsec-vpnd luci-app-ramfree luci-app-vsftpd luci-app-usb-printer luci-app-autoreboot luci-app-syncdial luci-app-eqos luci-app-nps luci-app-socat luci-app-n2n luci-app-softethervpn
+clone_dir master https://github.com/immortalwrt/luci luci-app-zerotier luci-app-openvpn-server luci-app-ipsec-vpnd luci-app-ramfree luci-app-vsftpd luci-app-usb-printer luci-app-autoreboot luci-app-syncdial luci-app-eqos luci-app-nps luci-app-socat luci-app-n2n luci-app-softethervpn luci-app-vlmcsd
 
 # 补全依赖
-clone_dir master https://github.com/immortalwrt/packages nps n2n socat strongswan
+clone_dir master https://github.com/immortalwrt/packages nps n2n socat strongswan vlmcsd
 
 # ddns-go 动态域名
 # clone_all https://github.com/sirpdboy/luci-app-ddns-go
