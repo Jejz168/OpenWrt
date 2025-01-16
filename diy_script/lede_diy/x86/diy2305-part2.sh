210f6774caa36da12b8f3712a2fbde3601c3162d
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-01-16 21:59:43 +0800

    更新 diy2305-part2.sh

--- diy_script/lede_diy/x86/diy2305-part2.sh
+++ diy_script/lede_diy/x86/diy2305-part2.sh
@@ -78,6 +78,9 @@ clone_dir main https://github.com/xiangfeidexiaohuo/2305-ipk luci-app-adguardhom
 # 替换immortalwrt插件
 clone_dir master https://github.com/immortalwrt/luci luci-app-syncdial luci-app-eqos luci-app-nps luci-app-frpc luci-app-frps luci-app-hd-idle luci-app-socat luci-app-n2n luci-app-softethervpn
 
+# 补全依赖
+clone_dir master https://github.com/immortalwrt/packages nps n2n socat strongswan
+
 # ddns-go 动态域名
 # clone_all https://github.com/sirpdboy/luci-app-ddns-go
 
