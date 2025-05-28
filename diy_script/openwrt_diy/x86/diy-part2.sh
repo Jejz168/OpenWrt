1f395acb2bf719c58f1173cd2213a98fd10ab585
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-05-29 06:27:10 +0800

    更新 diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -97,7 +97,7 @@ else
     # 替换immortalwrt插件
     clone_dir master https://github.com/immortalwrt/luci luci-app-zerotier luci-app-openvpn-server luci-app-ipsec-vpnd luci-app-ramfree luci-app-vsftpd luci-app-usb-printer luci-app-autoreboot luci-app-syncdial luci-app-eqos luci-app-nps luci-app-n2n luci-app-softethervpn luci-app-vlmcsd luci-app-hd-idle
     # 补全依赖
-    clone_dir master https://github.com/immortalwrt/packages zerotier nps n2n socat strongswan vlmcsd vsftpd hd-idle rust
+    clone_dir master https://github.com/immortalwrt/packages zerotier nps n2n socat strongswan vlmcsd vsftpd hd-idle
 fi
 
 # aria2 & ariaNG
