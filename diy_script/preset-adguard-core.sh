adfce7950c43cca4739d080048459713a4a91c57
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-07-10 18:35:16 +0800

    Update preset-adguard-core.sh

--- diy_script/preset-adguard-core.sh
+++ diy_script/preset-adguard-core.sh
@@ -4,7 +4,7 @@
 
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep "browser_download_url" | grep "AdGuardHome_linux_${1}.tar.gz" | cut -d '"' -f 4 | head -n1)
 
-wget -O /tmp/AdGuardHome.tar.gz "$AGH_CORE"
+wget -qO /tmp/AdGuardHome.tar.gz "$AGH_CORE"
 
 tar -xzvf /tmp/AdGuardHome.tar.gz -C files/usr/bin/AdGuardHome --strip-components=1
 
