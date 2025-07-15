d9b8ed673b7fa7de2fe797112387d17ada6d7a3c
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-07-15 13:41:05 +0800

    Update preset-adguard-core.sh

--- diy_script/preset-adguard-core.sh
+++ diy_script/preset-adguard-core.sh
@@ -2,10 +2,8 @@
 
 [ -d files/usr/bin/AdGuardHome ] || mkdir -p files/usr/bin/AdGuardHome
 
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep "browser_download_url" | grep "AdGuardHome_linux_${1}.tar.gz" | cut -d '"' -f 4 | head -n1)
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
 
-wget -qO /tmp/AdGuardHome.tar.gz "$AGH_CORE"
-
-tar -xzvf /tmp/AdGuardHome.tar.gz -C files/usr/bin/AdGuardHome --strip-components=1 AdGuardHome/AdGuardHome
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
 
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome
