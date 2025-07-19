aaf183862d012f362039a7f50d9d4b31e5194540
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-07-19 09:50:04 +0800

    更新 preset-adguard-core.sh

--- diy_script/preset-adguard-core.sh
+++ diy_script/preset-adguard-core.sh
@@ -6,10 +6,11 @@ AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/release
 
 if [ -z "$AGH_CORE" ]; then
   echo "❌ 未找到适用于 ${1} 的 AdGuardHome 下载链接！"
-  exit 1
+  AGH_CORE="https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.63/AdGuardHome_linux_${1}.tar.gz"
+  echo "✅ 已启用备用下载地址"
 fi
 
-echo "✅ 下载地址: $AGH_CORE"
+echo "📥 下载地址: $AGH_CORE"
 
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
 
