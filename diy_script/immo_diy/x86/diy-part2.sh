847ed2d30c1ebac000ee8d68fb21f7bf9a90460e
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-08-01 08:21:56 +0800

    更新 diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -72,7 +72,7 @@ sed -i '$a net.core.rmem_max=16777216' package/base-files/files/etc/sysctl.conf
 
 # 报错修复
 # sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
-if [ "$REPO_BRANCH" = "master" ]; then
+if [ "$REPO_BRANCH" = "openwrt-23.05" ]; then
     sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
 fi
 
