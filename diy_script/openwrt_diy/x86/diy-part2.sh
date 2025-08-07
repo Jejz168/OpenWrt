8d37f2f67dc2b10ae816913521c258eced68c126
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-08-07 19:19:56 +0800

    更新 diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -103,9 +103,9 @@ if [ "$REPO_BRANCH" = "openwrt-23.05" ]; then
     clone_dir openwrt-23.05 https://github.com/immortalwrt/packages zerotier nps n2n socat strongswan vlmcsd vsftpd hd-idle
 else
     # 替换immortalwrt插件
-    clone_dir master https://github.com/immortalwrt/luci luci-app-zerotier luci-app-openvpn-server luci-app-ipsec-vpnd luci-app-ramfree luci-app-vsftpd luci-app-usb-printer luci-app-autoreboot luci-app-syncdial luci-app-eqos luci-app-nps luci-app-n2n luci-app-softethervpn luci-app-vlmcsd luci-app-hd-idle
+    clone_dir master https://github.com/immortalwrt/luci luci-app-zerotier luci-app-openvpn-server luci-app-ipsec-vpnd luci-app-ramfree luci-app-vsftpd luci-app-usb-printer luci-app-autoreboot luci-app-syncdial luci-app-eqos luci-app-nps luci-app-n2n luci-app-softethervpn luci-app-vlmcsd luci-app-hd-idle luci-app-mwan3
     # 补全依赖
-    clone_dir master https://github.com/immortalwrt/packages zerotier nps n2n socat strongswan vlmcsd vsftpd hd-idle
+    clone_dir master https://github.com/immortalwrt/packages zerotier nps n2n socat strongswan vlmcsd vsftpd hd-idle mwan3
 fi
 
 # aria2 & ariaNG
