b41585b8501c184908d536f365f1de77952b3900
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-08-12 06:46:50 +0800

    Update diy2305-part2.sh

--- diy_script/lede_diy/x86/diy2305-part2.sh
+++ diy_script/lede_diy/x86/diy2305-part2.sh
@@ -156,9 +156,15 @@ make && sudo make install
 popd
 
 # argon 主题
-git_clone https://github.com/jerrykuku/luci-theme-argon
-git_clone https://github.com/jerrykuku/luci-app-argon-config
-# clone_all https://github.com/sbwml/luci-theme-argon
+# git_clone https://github.com/jerrykuku/luci-theme-argon
+# git_clone https://github.com/jerrykuku/luci-app-argon-config
+clone_all https://github.com/sbwml/luci-theme-argon
+
+# sbwml主题背景
+# 替换所有文件内容中的 bg.webp 为 bg1.jpg
+find "$destination_dir/luci-theme-argon" -type f -exec sed -i 's/bg\.webp/bg1.jpg/g' {} +
+# 替换所有文件名
+find "$destination_dir/luci-theme-argon" -type f -name "bg.webp" -exec bash -c 'mv "$1" "${1/bg.webp/bg1.jpg}"' _ {} \;
 
 # 更改argon主题背景
 # cp -f $GITHUB_WORKSPACE/personal/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
