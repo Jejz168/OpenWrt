2f3987a531148809b1193027b73f1f83a32a13ae
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-08-11 22:23:12 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -194,13 +194,15 @@ clone_all https://github.com/sbwml/luci-theme-argon
 git_clone https://github.com/sirpdboy/luci-theme-kucat
 git_clone https://github.com/sirpdboy/luci-app-kucat-config
 
+# sbwml主题背景
+find "$destination_dir/luci-theme-argon" -type f -exec sed -i 's/bg.webp/bg1.jpg/g' {} +
+
 ## argon主题设置
 # cp -f $GITHUB_WORKSPACE/personal/bg1.jpg $destination_dir/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 # 获取当天的星期几 (1=星期一, ..., 7=星期日)
 bg_file="bg$((($(date +%w) + 6) % 7 + 1)).jpg"
 # argon登录页面美化
-#ARGON_IMG_FILE="$destination_dir/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg"
-ARGON_IMG_FILE="$destination_dir/luci-theme-argon/htdocs/luci-static/argon/img/bg.webp"
+ARGON_IMG_FILE="$destination_dir/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg"
 if [ -f "$ARGON_IMG_FILE" ]; then
     # 替换Argon主题内建壁纸
     cp -f "$GITHUB_WORKSPACE/personal/$bg_file" "$ARGON_IMG_FILE"
