833b07a89b32c7ddb3b8fb59306a8c5e01cea941
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-08-12 06:44:46 +0800

    Update diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -158,12 +158,13 @@ make && sudo make install
 popd
 
 # 添加主题
+# git_clone https://github.com/sirpdboy/luci-theme-kucat
+# git_clone https://github.com/sirpdboy/luci-app-kucat-config
 # git_clone https://github.com/kiddin9/luci-theme-edge
 # git_clone https://github.com/jerrykuku/luci-theme-argon
 # git_clone https://github.com/jerrykuku/luci-app-argon-config
 clone_all https://github.com/sbwml/luci-theme-argon
-git_clone https://github.com/sirpdboy/luci-theme-kucat
-git_clone https://github.com/sirpdboy/luci-app-kucat-config
+
 
 # sbwml主题背景
 # 替换所有文件内容中的 bg.webp 为 bg1.jpg
