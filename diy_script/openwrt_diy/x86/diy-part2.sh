43a4355ef276e0b644f536cdf1072357cde1a1d9
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-08-12 06:26:15 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -195,7 +195,10 @@ git_clone https://github.com/sirpdboy/luci-theme-kucat
 git_clone https://github.com/sirpdboy/luci-app-kucat-config
 
 # sbwml主题背景
-find "$destination_dir/luci-theme-argon" -type f -exec sed -i 's/bg.webp/bg1.jpg/g' {} +
+# 替换所有文件内容中的 bg.webp 为 bg1.jpg
+find "$destination_dir/luci-theme-argon" -type f -exec sed -i 's/bg\.webp/bg1.jpg/g' {} +
+# 替换所有文件名
+find "$destination_dir/luci-theme-argon" -type f -name "bg.webp" -exec bash -c 'mv "$1" "${1/bg.webp/bg1.jpg}"' _ {} \;
 
 ## argon主题设置
 # cp -f $GITHUB_WORKSPACE/personal/bg1.jpg $destination_dir/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
