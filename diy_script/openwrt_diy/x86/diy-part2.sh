51f71d265da8874aa9298d48a622af5c3b055f72
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2025-09-27 09:02:58 +0800

    Update diy-part2.sh

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -85,6 +85,7 @@ sed -i '$a net.core.rmem_max=16777216' package/base-files/files/etc/sysctl.conf
 # rm -rf feeds/packages/utils/v2dat
 if [ "$REPO_BRANCH" != "openwrt-23.05" ]; then
     echo "开始修复报错……"
+    cp -f $GITHUB_WORKSPACE/personal/rust/* feeds/packages/lang/rust/Makefile
     sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
     echo "修复完成……"
 fi
