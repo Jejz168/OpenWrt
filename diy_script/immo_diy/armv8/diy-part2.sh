fd763f958bc38cb8fc2b997931a15a4cc953eb97
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2026-01-01 14:58:35 +0800

    更新 diy-part2.sh

--- diy_script/immo_diy/armv8/diy-part2.sh
+++ diy_script/immo_diy/armv8/diy-part2.sh
@@ -86,10 +86,12 @@ done
 
 # rust(ci false)
 if [ "$REPO_BRANCH" != "openwrt-23.05" ]; then
-  echo -n "Repair rust ..."
-  # sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
-  sed -i 's/--build-dir\ $(HOST_BUILD_DIR)\/build/--build-dir\ $(HOST_BUILD_DIR)\/build\ \\\n\		--ci\ false/' feeds/packages/lang/rust/Makefile
-  echo "✅"
+  if ! grep -q -- '--ci[[:space:]]\+false' feeds/packages/lang/rust/Makefile; then
+    echo -n "Repair rust ..."
+    # sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
+    sed -i 's/--build-dir\ $(HOST_BUILD_DIR)\/build/--build-dir\ $(HOST_BUILD_DIR)\/build\ \\\n\		--ci\ false/' feeds/packages/lang/rust/Makefile
+    echo "✅"
+  fi
 fi
 
 # 修复ramfree位置问题
