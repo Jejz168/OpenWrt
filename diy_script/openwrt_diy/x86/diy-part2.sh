0dd77367debe90720896490ed7848b8a1ab5176c
Jejz168 <53441247+Jejz168@users.noreply.github.com>
2026-01-04 19:17:20 +0800

    Add files via upload

--- diy_script/openwrt_diy/x86/diy-part2.sh
+++ diy_script/openwrt_diy/x86/diy-part2.sh
@@ -99,6 +99,13 @@ else
     git clone --depth=1 https://github.com/sbwml/autocore-arm.git package/system/autocore
 fi
 
+# autocore中CPU usage翻译
+cat >> "feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po" <<-EOF
+
+msgid "CPU usage"
+msgstr "CPU 使用率"
+EOF
+
 # 报错修复
 # rm -rf feeds/packages/utils/v2dat
 
@@ -236,8 +243,11 @@ git_clone https://github.com/sirpdboy/luci-app-kucat-config
 # git_clone https://github.com/kiddin9/luci-theme-edge
 # git_clone https://github.com/jerrykuku/luci-theme-argon
 # git_clone https://github.com/jerrykuku/luci-app-argon-config
-clone_all https://github.com/sbwml/luci-theme-argon
-
+if [ "$REPO_BRANCH" = "openwrt-23.05" ] || [ "$REPO_BRANCH" = "openwrt-24.10" ]; then
+    clone_all "$REPO_BRANCH" https://github.com/sbwml/luci-theme-argon
+else
+    clone_all https://github.com/sbwml/luci-theme-argon
+fi
 
 # sbwml主题背景
 # 替换所有文件内容中的 bg.webp 为 bg1.jpg
